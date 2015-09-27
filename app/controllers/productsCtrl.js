ae08087e1b18dcc5199a06a43bd5475a2bd7acab
Rad Zajaczkowski <rad@mira-labs.com>
2015-09-27 21:28:48 +0100

    ng-show, ng-click

--- app/controllers/productsCtrl.js
+++ app/controllers/productsCtrl.js
@@ -2,4 +2,10 @@ angular.module('myApp').controller('productsCtrl',function($scope,$http){
     $http.get('http://design2taste.com/angular/products.php').success(function(response){
         $scope.products = response;
     });
+
+    $scope.showCatalog = 0;
+
+    $scope.toggle = function(){
+        $scope.showCatalog = !$scope.showCatalog;
+    }
 });
